66a5f82126ad9ff0694a877e45114cff36597f9b
Jota Perez <yojota@gmail.com>
2014-03-31 13:45:55 -0300

    Conector a base de datos

--- conector.php
+++ conector.php
@@ -1,6 +1,5 @@
 <?php 
 //conector a la base de datos
-	echo "hola mundo";
 	$host="127.0.0.1";
 	$port=3306;
 	$socket="";
@@ -8,8 +7,29 @@
 	$password="jotajota";
 	$dbname="clinica";
 
-	$con = new mysqli($host, $user, $password, $dbname, $port, $socket)
-	or die ('Could not connect to the database server' . mysqli_connect_error());
+	$con = mysql_connect($host,$user,$password);
+	if (!$con) {
+    	echo "No pudo conectarse a la BD: " . mysql_error();
+    	exit;
+	}
 
-//$con->close();
+	$sql = "SELECT idPersonas, apellido, nombre, email, telefono, staff FROM `clinica`.`Personas`";
+	$resultado = mysql_query($sql);
+	
+	if (!$resultado) {
+    	echo "No se pudo ejecutar con exito la consulta ($sql) en la BD: " . mysql_error();
+    	exit;
+	}
+	if (mysql_num_rows($resultado) == 0) {
+    echo "No se han encontrado filas, nada a imprimir, asi que voy a detenerme.";
+    exit;
+	}
+	while ($fila = mysql_fetch_assoc($resultado)) {
+    	print ($fila["apellido"]." ");
+    	print ($fila["nombre"]." ");
+    	print ($fila["email"]." ");
+    	print("<br>");
+	}
+
+	mysql_free_result($resultado);
 ?>
